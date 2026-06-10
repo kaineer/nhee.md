7f57c8fc626a16e436a5d254c454c30715a01a42
Sergey Kluchkovsky <kaineer@gmail.com>
2026-06-10 17:14:30 +0500

    dump changes

--- bin/kit/tag.py
+++ bin/kit/tag.py
@@ -6,8 +6,7 @@ def _children(ch=None):
     if ch is None:
         return ""
     if type(ch) is list:
-        return "".join([str(c) for c in ch 
-                        if c is not None])
+        return "".join([str(c) for c in ch if c is not None])
     if type(ch) is str:
         return ch
 
@@ -17,16 +16,18 @@ def _classname(cn=None):
         return ""
     return f' class="{cn}"'
 
+
 def is_empty_by_design(tag_name):
-    return (
-        tag_name in ["hr", "link", "img", "input", "meta"]
-    )
+    return tag_name in ["hr", "link", "img", "input", "meta"]
+
 
 def open_tag(name, classname, attr):
     return f"<{name}{_classname(classname)}{attributes(attr)}>"
 
+
 def close_tag(name):
     return f"</{name}>"
+
 
 def tag(name, classname=None, attr={}, children=None):
     parts = [open_tag(name, classname, attr)]
@@ -34,5 +35,6 @@ def tag(name, classname=None, attr={}, children=None):
         parts.append(_children(children))
         parts.append(close_tag(name))
     return "".join(parts)
+
 
 __all__ = ["tag"]
