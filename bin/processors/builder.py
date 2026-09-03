950963eef2723051a7c58076106bbfd19200c5fd
Sergey Kluchkovsky <kaineer@gmail.com>
2026-09-03 16:57:13 +0500

    Save on 2026.09.03 -- 16:57:13

--- bin/processors/builder.py
+++ bin/processors/builder.py
@@ -10,6 +10,7 @@ from page.types.dummy import Dummy
 from primitives.context.data import ContextLoader
 from primitives.navbar.data import NavbarData
 from primitives.page_enumerate import find_meta_files
+from jinja2 import Template
 from primitives.template.template_container import TemplateContainer
 from widgets.navbar import build_navbar
 
@@ -29,24 +30,30 @@ class Builder:
         self.context_loader = ContextLoader(root)
         self.navbar_data = NavbarData(str(Path(root) / "navbar.yaml"))
 
+    def _jinja_page_template(self, type):
+        path = Path("jinja") / "pages" / f"{type}.html.j2"
+        if not path.is_file():
+            return None
+        return Template(path.read_text())
+
     def build_page(self, file):
         context = self.context_loader.load(file)
         navbar = build_navbar(context, self.navbar_data)
         type = context.type
         title = context.title
-        template = self.templates.get(type)
         page_class = page_types[type]
         parameters = page_class(context).parameters()
-        page = str(
-            template.apply(
-                {
-                    "navbar": navbar,
-                    **parameters,
-                    "root": context.root,
-                    "title": title,
-                }
-            )
-        )
+        mappings = {
+            "navbar": navbar,
+            **parameters,
+            "root": context.root,
+            "title": title,
+        }
+        jinja_template = self._jinja_page_template(type)
+        if jinja_template is not None:
+            return jinja_template.render({**mappings, "page": {"title": title}})
+        template = self.templates.get(type)
+        page = str(template.apply(mappings))
         return page
 
     def build(self):
